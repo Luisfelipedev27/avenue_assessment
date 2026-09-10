d9951041197091629634c63586348b10fb845e54
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2026-09-10 20:49:09 -0300

    Setup rspec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module Avenue
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.generators do |generate|
+      generate.test_framework(
+        :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: true
+      )
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
